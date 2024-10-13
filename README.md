c11fef21e939ffb474c30d4223f2f107421facfd
sahil <sahil29roy@gmail.com>
2024-10-13 16:08:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sahil's Portfolio
+
+Welcome to my personal portfolio! I'm Sahil, a passionate web developer and a 2nd-year student at Lovely Professional University (LPU), Jalandhar. This portfolio showcases my skills, projects, and ways to get in touch with me.
+
+## About Me
+
+I'm a web developer skilled in the MERN stack (MongoDB, Express, React, Node.js) and proficient in CSS frameworks like Bootstrap and Tailwind CSS. I enjoy creating seamless user experiences and building robust web applications. In addition to web development, I have experience in UI/UX design using tools like Canva.
+
+
+## Contact Me
+
+If you're interested in collaborating, have questions, or just want to say hi, feel free to reach out!
+
+- **Email**: sahil@example.com
+- **LinkedIn**: [www.linkedin.com/in/sahil29roy](#)
+- **GitHub**: [https://github.com/sahil29roy](#)
+
+Thank you for visiting my portfolio! I'm excited to connect and share my work.
---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "SAHIL";
+const meta = {
+    title: "Sahil",
+    description: "I’m Sahil _ Full stack devloper,currently studing in LPU",
+};
+
+const introdata = {
+    title: "I’m Sahil",
+    animated: {
+        first: "I love coding",
+        second: "I'm a Web Developer",
+    },
+    description: "I'm Sahil, a passionate web developer and a 2nd-year student at LPU, Jalandhar. Coding is my passion, and I love working on creative projects.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+
+const dataabout = {
+    title: "A Bit About Myself",
+    aboutme: "I'm Sahil from Deoghar, currently pursuing my 2nd year at Lovely Professional University, Jalandhar. I specialize in web development and enjoy coding and solving technical challenges.",
+};
+
+const worktimeline = [{
+        jobtitle: "Internship as a developer",
+        where: "Brdztech infosystem,Ranchi",
+        date: "Oct/2023 - nov/2023",
+    },
+    {
+        jobtitle: "Designer at GetDev",
+        where: "Remort",
+        date: "March 2024",
+    },
+    
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Mongodb",
+        value: 60,
+    },
+    {
+        name: "ExpressJs",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "NodeJs",
+        value: 85,
+    },
+];
+
+const services = [
+    {
+        title: "UI & UX Design",
+        description: "I specialize in creating intuitive and responsive user interfaces using CSS frameworks like Bootstrap and Tailwind CSS. I also utilize tools like Canva to design visually appealing and user-friendly experiences.",
+    },
+    {
+        title: "Web Development",
+        description: "As a web developer proficient in the MERN stack (MongoDB, Express, React, Node.js), I build dynamic and efficient websites and web applications that focus on performance and scalability.",
+    },
+    {
+        title: "WordPress Design",
+        description: "I offer custom WordPress design services, creating tailored websites that meet the unique needs of clients, ensuring both functionality and an engaging user experience.",
+    }
+];
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sahil08roy@gmail.com",
+    YOUR_FONE: "6206505126",
+    description: "I'm Sahil, a passionate web developer and a 2nd-year student at LPU, Jalandhar. Coding is my passion, and I love working on creative projects. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
